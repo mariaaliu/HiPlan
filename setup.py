26c32ae68437e5617b1190b685969f69b21b2986
mariaaliu <almade987@gmail.com>
2021-04-09 20:46:17 +0300

    Test1

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+    name="hiplan",
+    version="0.0.1",
+    author="<COMPLETE HERE>",
+    author_email="<COMPLETE HERE>",
+    description="<COMPLETE HERE>",
+    url="https://github.com/<COMPLETE HERE>",
+    project_urls={
+        "Bug Tracker": "https://github.com/<COMPLETE HERE>/issues",
+    },
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
