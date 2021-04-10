16a5c35ac894b61b14ebde2f9d09d848be8060cb
mariaaliu <almade987@gmail.com>
2021-04-10 16:40:33 +0300

    Am importat App, Board etc in app.py

--- hiplan/app.py
+++ hiplan/app.py
@@ -2,6 +2,12 @@ from typing import List
 import enum
 from datetime import datetime
 import json
+from base.app.py import App
+from base.board.py import Board
+from base.task.py import Task
+from base.member.py import Member
+from base.record.py import Record
+from base.priority.py import Priority 
 
 app = App()
 app.add_board()
--- hiplan/base/board.py
+++ hiplan/base/board.py
@@ -1,3 +1,4 @@
+
 class Board:
     def __init__(self, name = "Untitled", records = None, description = "", members = None):
         self.name = name
