db757c6b3cead97a937684da4a9f48034d78d0e1
mariaaliu <almade987@gmail.com>
2021-04-15 11:12:39 +0300

    Nu facusem pe master modificarile

--- hiplan/base/member.py
+++ hiplan/base/member.py
@@ -1,4 +1,28 @@
 import enum
+from hiplan.base.task import Task
 
 class Member:
-    pass
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def solve_task(self, task: Task):
+        if not isinstance(task, Task):
+            raise Exception("{} is not a task".format(task))
+
+        if task.progress_status is ProgressStatus.SOLVED:
+            raise Exception("The task was already solved")
+        else:
+            task.progress_status = ProgressStatus.SOLVED
+    
+    def serialize(self)
+        return {
+            'name': self.name,
+            'email': self.email
+        }
+
+    @staticmethod
+    def deserialize(dictionary: dict):
+        member = Member(name=dictionary['name'], email=dictionary['email'])
+
+        return member
