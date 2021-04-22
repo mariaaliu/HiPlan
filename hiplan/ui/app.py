cb5590bc904c9ccddea19e326f9ef83ad2e6e042
maria <almade987@gmail.com>
2021-04-22 11:36:11 +0300

    Marti

---
+++ hiplan/ui/app.py
@@ -0,0 +1,26 @@
+from kivymd.app import MDApp as App
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.relativelayout import RelativeLayout
+
+
+class BoardScreen(Screen):
+    pass
+
+class BoardIcon(RelativeLayout):
+    pass
+
+
+class HiPlanApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen_manager = ScreenManager()
+
+    def build(self):
+        self.screen_manager.add_widget(screen=BoardScreen(name='board-screen'))
+        self.screen_manager.current = 'board-screen'
+        return self.screen_manager
+
+    
+    
+HiPlanApp().run()
